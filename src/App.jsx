f18ca51edf24e034199a96220ad509628bf93b6b
Prince <princekashishbbu@gmail.com>
2024-07-24 01:29:27 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import "./App.css";
-import { Home } from "./Pages";
 import { Footer, Navbar } from "./Components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
