646edfd9d106be4bf38a535c57bdf04226b77f0d
Prince <princekashishbbu@gmail.com>
2024-07-24 01:27:44 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 import { Home } from "./Pages";
+import { Footer, Navbar } from "./Components";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-<>
-<div className="bg-[#EEF7F8]">
- <Navbar />
- <main>
-   <Outlet />
- </main>
- <Footer />
-</div>
-</>
-   
+    <>
+      <div className="bg-[#EEF7F8]">
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
