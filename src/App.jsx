6345645999bd0b31382b5ca193f9aa9ac7499c64
Prince <princekashishbbu@gmail.com>
2024-07-23 15:53:27 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react' 
-import './App.css'
-import { Home } from './Pages'
+import { useState } from "react";
+import "./App.css";
+import { Home } from "./Pages";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-       <Home/>
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
