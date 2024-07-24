81861f0c588324978023c4d95fe4175e329118c2
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-24 19:04:08 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,13 @@
 import logo from "./logo.png"
+import signup from "./signup.png"
 import location from "./location.svg"
 import mail from "./mail.svg"
 import phone from "./phone.svg"
 
+
 export{
     logo,
+    singup,
     location,
     mail,
     phone
