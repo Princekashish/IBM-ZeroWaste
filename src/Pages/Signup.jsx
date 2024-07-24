deac6b35978c47d665729af2559b59f1615e81f1
Prashant Kumar <email2prashantkr@gmail.com>
2024-07-25 01:02:43 +0530

    Signup modified

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -4,7 +4,7 @@ import { signup } from '../assets';
 function Signup() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center p-4">
-        <div className="w-full max-w-4xl flex flex-col md:flex-row bg-white shadow-2xl rounded-lg overflow-hidden  shadow-slate-800">
+        <div className="w-full max-w-4xl flex flex-col md:flex-row bg-white shadow-2xl rounded-lg overflow-hidden drop-shadow-2xl  shadow-slate-950">
             <div className="w-full md:w-1/2 bg-[#E6511A] flex flex-col justify-center items-center p-8 ">
                 <img
                     src= {signup}
@@ -13,38 +13,38 @@ function Signup() {
                 />
                 <h2 className="text-white text-3xl font-bold mt-4">Welcome to Sign Up</h2>
             </div>
-            <div className="w-full md:w-1/2 p-8 bg-slate-200 ">
+            <div className="w-full md:w-1/2 p-8 bg-[#DCDCDC] ">
                 <h2 className="text-2xl font-bold mb-4">SIGN UP</h2>
                 <form className="space-y-4">
                     <div className="flex flex-col md:flex-row md:space-x-4 ">
                         <input
                             type="text"
                             placeholder="Enter Name "
-                            className="w-full md:w-1/2 p-2 pl-4 mb-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#eebeac]"
+                            className="w-full md:w-1/2 p-2 pl-4 mb-2 text-sm  border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-[#eebeac]"
                             required
                         />
                         <input
                             type="email"
                             placeholder="Email ID"
-                            className="w-full md:w-1/2 p-2 pl-4 mb-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#eebeac]"
+                            className="w-full md:w-1/2 p-2 pl-4 mb-2 text-sm border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-[#eebeac]"
                             required
                         />
                     </div>
                     <input
                         type="text"
                         placeholder="Create Username"
-                        className="w-full p-2 pl-4 mb-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#eebeac]"
+                        className="w-full p-2 pl-4 mb-2 text-sm border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-[#eebeac]"
                         required
                     />
                     <input
                         type="password"
                         placeholder="Create Password"
-                        className="w-full p-2 pl-4 mb-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#eebeac]"
+                        className="w-full p-2 pl-4 mb-2 text-sm border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-[#eebeac]"
                         required
                     />
                     <button
                         type="submit"
-                        className="w-full bg-[#E6511A] text-white py-2 mb-2 rounded-md font-bold hover:bg-[#cf4614] transition duration-200"
+                        className="w-full bg-[#E6511A] text-white py-2 mb-2 rounded-full font-bold hover:bg-[#cf4614] transition duration-200"
                     >
                         SIGN UP
                     </button>
