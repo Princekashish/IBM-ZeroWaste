8ad03f3f21d4ebfaa86e078211cf934733396895
Prince <princekashishbbu@gmail.com>
2024-07-24 22:52:56 +0530

    change

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { logo } from "../../assets";
-// import { Link } from "react-dom";
 import { Button } from "../index";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoCloseSharp } from "react-icons/io5";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 function Navbar() {
   const [toggle, setToggle] = useState(false);
 
@@ -30,8 +29,8 @@ function Navbar() {
   return (
     <div className=" bg-[#EEF7F8] lg:p-6 p-3 flex justify-between items-center w-full">
       <div>
-        <Link to='/'>
-        <img src={logo} alt="logo" className="lg:max-h-14  max-h-9  " />
+        <Link to="/">
+          <img src={logo} alt="logo" className="lg:max-h-14  max-h-9  " />
         </Link>
       </div>
 
@@ -40,18 +39,21 @@ function Navbar() {
           {navItems.map((item, index) => {
             return (
               <Link to={item.link}>
-              <div
-                key={index}
-                className="font-bold text-xl ]cursor-pointer text-[#484848]"
+                <div
+                  key={index}
+                  className="font-bold text-xl ]cursor-pointer text-[#484848]"
                 >
-                <h4>{item.name}</h4>
-              </div>
-                </Link>
+                  <h4>{item.name}</h4>
+                </div>
+              </Link>
             );
           })}
         </div>
         <div>
-          <Button msg="LOGIN" />
+          <Link to="/Signup">
+            {" "}
+            <Button msg="LOGIN" />
+          </Link>
         </div>
       </div>
 
@@ -65,11 +67,14 @@ function Navbar() {
             )}
           </button>
         </div>
-        <div className={` ${toggle ? "flex" : "hidden"} p-3   absolute top-20 right-0 mx-4 my-2 min-w-[140px] border-2 border-black rounded-xl  `}>
+        <div
+          className={` ${
+            toggle ? "flex" : "hidden"
+          } p-3   absolute top-20 right-0 mx-4 my-2 min-w-[140px] border-2 border-black rounded-xl  `}
+        >
           <div className="   w-full ">
             {navItems.map((item, index) => {
               return (
-                
                 <div
                   key={index}
                   className="font-bold text-xl ]cursor-pointer text-[#484848]"
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,4 @@
 import logo from "./logo.png"
-import signup from "./signup.png"
 import location from "./location.svg"
 import mail from "./mail.svg"
 import phone from "./phone.svg"
@@ -7,7 +6,6 @@ import phone from "./phone.svg"
 
 export{
     logo,
-    singup,
     location,
     mail,
     phone
