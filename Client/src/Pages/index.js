710e7a55b1d1f254e115ef1d9c7743c220528791
Prince Kashish <65589108+Princekashish@users.noreply.github.com>
2024-07-29 11:21:08 +0530

    Delete Client directory

--- Client/src/Pages/index.js
+++
@@ -1,8 +0,0 @@
-import Contact from "./Contactus.jsx";
-import Donation from "./Donation.jsx";
-import Education from "./Education.jsx";
-import Login from "./Login.jsx";
-import Signup from "./Signup.jsx";
-import Landingpage from "./Landingpage.jsx";
-
-export { Landingpage, Contact, Donation, Education, Login, Signup };
--- Client/src/utils/tost.js
+++
@@ -1,9 +0,0 @@
-import { toast } from "react-toastify";
-export const handleSucess = (message) => {
-  toast.success(message);
-};
-export const handlError = (msg) => {
-  toast.error(msg, {
-    position: "top-right",
-  });
-};
